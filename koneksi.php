834d0bcf12dec0f7f02899d5c9375e148879100d
bagusnata330 <bagusnata330@gmail.com>
2022-06-25 15:36:53 +0800

    Apply OOP concept to connection system

---
+++ koneksi.php
@@ -0,0 +1,21 @@
+<?php
+  class database{
+    
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "bo203_db";
+    
+    protected $conn;
+
+    public function __construct()
+    {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+        if (!$this->conn){
+          echo "Gagal terhubung ke MySQL: " . mysqli_connect_error();
+          exit;
+        }
+        return $this->conn;
+     } 
+  }
+?>
