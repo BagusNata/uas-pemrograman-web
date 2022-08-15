d5321e0cfe51b602a8998f286b6127bc2f75727b
bagusnata330 <bagusnata330@gmail.com>
2022-08-15 10:55:06 +0800

    create new navbar for mahasiswa

---
+++ navbar_mhs.php
@@ -0,0 +1,32 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark ">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse mynavbar" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <!-- Mahasiswa -->
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="selectMhs.php">Mahasiswa</a>
+        </li>
+        <!-- Dosen -->
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="selectDosen.php">Dosen</a>
+        </li>
+        <!-- Jurusan -->
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="selectJurusan.php">Jurusan</a>
+        </li>
+        <!-- Jadwal Kuliah -->
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="selectMhsJadwal.php">Jadwal Kuliah</a>
+        </li>
+         <!-- Admin -->
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="login_admin.php">Login as Admin</a>
+        </li>
+        <!-- Logout -->
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="logout.php">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
