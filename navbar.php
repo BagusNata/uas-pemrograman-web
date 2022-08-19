df738a92e8d13aaa88ba82a24466e72cfacc514b
bagusnata330 <bagusnata330@gmail.com>
2022-08-19 12:03:35 +0800

    initialize new navbar structure

--- navbar.php
+++ navbar.php
@@ -2,41 +2,34 @@
   <div class="container-fluid">
     <div class="collapse navbar-collapse mynavbar" id="navbarNavDropdown">
       <ul class="navbar-nav">
-        <!-- Home -->
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        </li>
-        <!-- Insert -->
+        <!-- Mahasiswa -->
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Insert
+            Mahasiswa
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="insertMhs.php">Data Mahasiswa</a></li>
-            <li><a class="dropdown-item" href="insertJurusan.php">Data Jurusan</a></li>
-            <li><a class="dropdown-item" href="insertDosen.php">Data Dosen</a></li>
+            <li><a class="dropdown-item" href="admin_selectMhs.php">Lihat Data Mahasiswa</a></li>
+            <li><a class="dropdown-item" href="insertMhs.php">Tambah Data Mahasiswa</a></li>
           </ul>
         </li>
-        <!-- Update -->
+        <!-- Jurusan -->
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Update
+            Jurusan
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="updateMhs.php">Data Mahasiswa</a></li>
-            <li><a class="dropdown-item" href="updateJurusan.php">Data Jurusan</a></li>
-            <li><a class="dropdown-item" href="updateDosen.php">Data dosen</a></li>
+            <li><a class="dropdown-item" href="admin_selectJurusan.php">Lihat Data Jurusan</a></li>
+            <li><a class="dropdown-item" href="insertJurusan.php">Tambah Data Jurusan</a></li>
           </ul>
         </li>
-        <!-- Delete -->
+        <!-- Dosen -->
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Delete
+            Dosen
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="deleteMhs.php">Data Mahasiswa</a></li>
-            <li><a class="dropdown-item" href="deleteJurusan.php">Data Jurusan</a></li>
-            <li><a class="dropdown-item" href="deleteDosen.php">Data dosen</a></li>
+            <li><a class="dropdown-item" href="admin_selectDosen.php">Lihat Data Dosen</a></li>
+            <li><a class="dropdown-item" href="insertDosen.php">Tambah Data Dosen</a></li>
           </ul>
         </li>
         <!-- Jadwal Kuliah -->
